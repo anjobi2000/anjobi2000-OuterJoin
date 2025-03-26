7a4e8307eaa601d6e3ad156381550860813f193c
Angela Jobi <Angela.chemmazham@gmail.com>
2025-03-26 16:48:09 +0000

    Completed Coding Lab Outer Join

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+SELECT * 
+FROM class c
+LEFT OUTER JOIN student s ON c.class_title = s.class_title;
---
+++ problem2.sql
@@ -0,0 +1,3 @@
+SELECT * 
+FROM class c
+RIGHT OUTER JOIN textbook t ON c.class_title = t.class_title;
